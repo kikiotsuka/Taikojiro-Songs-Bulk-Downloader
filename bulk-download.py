5c74fac0b28ea4500bd447ee8381ae9113ff0bd1
Mitsuru Otsuka <mitsuru.otsuka@gmail.com>
2017-03-09 02:57:29 +0900

    Added progress logging and error reports

--- bulk-download.py
+++ bulk-download.py
@@ -1,19 +1,28 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+logging = open('bulk-download-progress.csv')
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'}
-with open('links-custom-filtered-out.csv') as f:
+with open('links.csv') as f:
     next(f)
     for line in f:
         title, url, comment, downloads = line.split('|')
         print('Processing:', title, url, comment, downloads)
-        with requests.Session() as session:
-            session.headers.update(headers)
-            r = session.get(url)
-            soup = bs(r.content)
-            post_data = {'token': soup.find('form', {'name': 'agree'}).input.get('value')}
-            response = session.post(url, data=post_data)
-            dl_link = bs(response.content).find('a', {'class': 'download'}).get('href')
-            dl = session.get(dl_link, stream=True)
-            with open(title, 'wb') as zip_file:
-                zip_file.write(dl.content)
+        try:
+            with requests.Session() as session:
+                session.headers.update(headers)
+                r = session.get(url)
+                soup = bs(r.content)
+                post_data = {'token': soup.find('form', {'name': 'agree'}).input.get('value')}
+                response = session.post(url, data=post_data)
+                dl_link = bs(response.content).find('a', {'class': 'download'}).get('href')
+                dl = session.get(dl_link, stream=True)
+                with open(title, 'wb') as zip_file:
+                    zip_file.write(dl.content)
+                logging.print(line + '\n')
+        except Exception as e:
+            print('Error encountered while processing', title)
+            print(e)
+
+logging.close()
