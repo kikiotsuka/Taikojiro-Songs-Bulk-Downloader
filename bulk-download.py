26024aa5e5b8f7cbeb0679d867a7d635d17b3ec6
Mitsuru <mitsuru.otsuka@gmail.com>
2017-03-13 00:24:56 +0900

    fixed stuff

--- bulk-download.py
+++ bulk-download.py
@@ -1,27 +1,90 @@
-import requests
 from bs4 import BeautifulSoup as bs
+from io import BytesIO
+import requests
+import zipfile
+import os
 
-logging = open('bulk-download-progress.csv')
+FILE_LOCATIONS = ['fumen/arcade/', 'fumen/easy/', 'fumen/custom/', 'fumen/other/']
+PROGRESS_FILE = 'bulk-download-progress.csv'
+SKIP = -1
+ARCADE = 0
+EASY = 1
+CUSTOM = 2
+OTHER = 3
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'}
-with open('links.csv') as f:
-    for line in f:
-        title, comment, downloads, size_mb, link = line.split('|')
-        print('Processing:', title, url, comment, downloads)
-        try:
-            with requests.Session() as session:
-                session.headers.update(headers)
-                r = session.get(url)
-                soup = bs(r.content)
-                post_data = {'token': soup.find('form', {'name': 'agree'}).input.get('value')}
-                response = session.post(url, data=post_data)
-                dl_link = bs(response.content).find('a', {'class': 'download'}).get('href')
-                dl = session.get(dl_link, stream=True)
-                with open(title, 'wb') as zip_file:
-                    zip_file.write(dl.content)
-                logging.print(line + '\n')
-        except Exception as e:
-            print('Error encountered while processing', title)
-            print(e)
+def __main__():
+    completed = []
+    if os.path.exists(PROGRESS_FILE):
+        with open(PROGRESS_FILE, 'r') as f:
+            completed.append(f.read().split('\n'))
 
-logging.close()
+    setup()
+
+    progress = open(PROGRESS_FILE, 'w')
+
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'}
+    with open('links.csv') as flinks:
+        for line in flinks:
+            # If the file has been downloaded before, skip it
+            if line in completed:
+                continue
+            title, comment, downloads, size_mb, url = line.split('|')
+            print('Processing:', title, url, comment, downloads)
+            try:
+                with requests.Session() as session:
+                    session.headers.update(headers)
+                    r = session.get(url)
+                    soup = bs(r.content)
+                    post_data = {'token': soup.find('form', {'name': 'agree'}).input.get('value')}
+                    response = session.post(url, data=post_data)
+                    dl_link = bs(response.content).find('a', {'class': 'download'}).get('href')
+                    dl = session.get(dl_link, stream=True)
+
+                    zfile = zipfile.ZipFile(BytesIO(dl.content))
+                    status = process(zfile, comment, downloads)
+
+                    dirname = FILE_LOCATIONS[status] + title
+                    if not os.path.exists(dirname):
+                        os.makedirs(dirname)
+                    for f in zfile.infolist():
+                        ext = f.filename.split('.')[-1]
+                        data = zfile.read(f)
+                        with open(dirname + '/' + title + '.' + ext, 'wb') as fw:
+                            fw.write(data)
+                    progress.write(line + '\n')
+
+            except Exception as e:
+                print('Error encountered while processing', title)
+                print(e)
+                import sys
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(exc_type, fname, exc_tb.tb_lineno)
+                import pdb
+                pdb.set_trace()
+
+    progress.close()
+
+def setup():
+    for f in FILE_LOCATIONS:
+        if not os.path.exists(f):
+            os.makedirs(f)
+
+def process(zfile, comment, downloads):
+    if '新AC' in comment:
+        return ARCADE
+    if int(downloads) < 2000:
+        return SKIP
+    with open(zfile.namelist()[1], 'r') as f:
+        for line in f:
+            if line.startswith('LEVEL'):
+                stars = int(line.split(' ')[1])
+                if stars < 7:
+                    return EASY
+            break
+    if '創作' in comment:
+        return CUSTOM
+    return OTHER
+
+if __name__ == '__main__':
+    __main__()
--- scrape.py
+++ scrape.py
@@ -1,44 +1,50 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-base = ['http://ux.getuploader.com/e2339999zp/', 'http://ux.getuploader.com/e2337650/', 'http://ux.getuploader.com/toukyuutoyoko9000/', 'http://ux.getuploader.com/e2351000/']
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'}
-all_links = []
+def __main__():
+    base = ['http://ux.getuploader.com/e2339999zp/', 'http://ux.getuploader.com/e2337650/', 'http://ux.getuploader.com/toukyuutoyoko9000/', 'http://ux.getuploader.com/e2351000/']
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0'}
+    all_links = []
 
-for b in base:
-    print('===== SCRAPING BASE PAGE =====', b)
-    for idx in range(1, 999):
-        url = b + 'index/' + str(idx) + '/date/desc'
-        print('=== Sub Scrape ===', url)
-        base_done = False
-        try:
-            r = requests.get(url, headers=headers)
-            html = r.content
-            soup = bs(html)
-            rows = soup.find_all('table', class_='table table-small-font table-hover')[0].find_all('tr')[1:]
-            
-            if rows:
-                for row in rows:
-                    link = row.a.get('href')
+    for b in base:
+        print('===== SCRAPING BASE PAGE =====', b)
+        for idx in range(1, 999):
+            url = b + 'index/' + str(idx) + '/date/desc'
+            print('=== Sub Scrape ===', url)
+            base_done = False
+            try:
+                r = requests.get(url, headers=headers)
+                html = r.content
+                soup = bs(html)
+                rows = soup.find_all('table', class_='table table-small-font table-hover')[0].find_all('tr')[1:]
+                
+                if rows:
+                    for row in rows:
+                        link = row.a.get('href')
 
-                    td_list = list(map(lambda x: x.contents[0], row.find_all('td')))
+                        td_list = list(map(lambda x: x.contents[0], row.find_all('td')))
 
-                    title = row.a.get('title')
-                    comment = td_list[1]
-                    downloads = td_list[-2]
-                    size_mb = td_list[3]
-                    if (size_mb.split(' ')[1] == 'KB'):
-                        size_mb = str(float(size_mb.split(' ')[0]) / 1000.0) + ' MB'
-                    all_links.append('|'.join(map(str, [title, comment, downloads, size_mb, link])))
-                    print(all_links[-1])
-            else:
+                        title = row.a.get('title').split('.zip')[0]
+                        comment = td_list[1]
+                        downloads = td_list[-2]
+                        size_mb = td_list[3].split(' ')
+                        size_mb[0] = float(size_mb[0])
+                        if (size_mb[1] == 'KB'):
+                            size_mb[0] /= 1000.0
+                        size_mb = size_mb[0]
+                        all_links.append('|'.join(map(str, [title, comment, downloads, size_mb, link])))
+                        print(all_links[-1])
+                else:
+                    base_done = True
+            except Exception as e:
+                print(e)
                 base_done = True
-        except Exception as e:
-            print(e)
-            base_done = True
-        if base_done:
-            break
+            if base_done:
+                break
 
-with open('links.csv', 'w') as f:
-    for l in all_links:
-        f.write(l + '\n')
+    with open('links.csv', 'w') as f:
+        for l in all_links:
+            f.write(l + '\n')
+
+if __name__ == '__main__':
+    __main__()
